304348d3509baad5c1bd7a99e7f65299ec206c1f
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-12-08 19:06:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use("/cart", cartRouter);
 app.use("/payments", paymentRouter); 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
       console.log(`Server is running on https://localhost:${PORT}`);
     });
