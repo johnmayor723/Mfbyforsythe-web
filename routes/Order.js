dd04813f275c1687a23578f3e2dab3f112c4ad76
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-03-02 14:51:19 +0100

    Update Order.js

--- routes/Order.js
+++ routes/Order.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/order");
+const Order = require("../models/Order");
 
 // 1. Create a new order
 router.post("/create", async (req, res) => {
