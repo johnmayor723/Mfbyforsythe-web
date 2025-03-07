3f169a5ab61d52a3840057d8cca086e5da3727ac
may <may@xymail.com>
2025-03-07 20:31:55 +0100

    mnn

--- routes/payment.js
+++ routes/payment.js
@@ -44,8 +44,8 @@ router.post('/charge',  function(req, res, next) {
   var totalAmount = cart.totalAmount;
   console.log("cart found:", cart)
   console.log("Total amount is:", totalAmount)
-  
-  var stripe = require("stripe")("sk_test_51JJP3QC3AvHrSrpnTHIwGAdjNtZXURE2Ii8rHkMpbqQsxOfHFIcoHNcuZBGzpHX6FdgG1ZPXJoBp4ma8cS743WA500QRAG8n1g")
+  let sk  = "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"
+  var stripe = require("stripe")("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")
 
   stripe.charges.create({
       amount: totalAmount * 100 ,
